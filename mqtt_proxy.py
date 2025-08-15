63c8fc5f9ed0ac663dc941f4c7d29d16b130d302
M9204 <fattahvic@gmail.com>
2025-08-15 09:29:18 +0300

    Update mqtt_proxy.py

--- mqtt_proxy.py
+++ mqtt_proxy.py
@@ -1,33 +1,38 @@
-import socket, ssl, threading, os
+import socket, ssl, threading
 
 LISTEN_HOST = "0.0.0.0"
-LISTEN_PORT = int(os.environ.get("PORT", 5000))  # Render sets PORT env
-MQTT_HOST = "ddf927fd9af44789b245774345c7bf14.s1.eu.hivemq.cloud"
-MQTT_PORT = 8883
+LISTEN_PORT = 5000
+
+BROKER_HOST = "dog.lmq.cloudamqp.com"
+BROKER_PORT = 5671  # TLS
 
 def handle_client(client_socket):
-    context = ssl.create_default_context()
-    broker_socket = context.wrap_socket(socket.socket(), server_hostname=MQTT_HOST)
-    broker_socket.connect((MQTT_HOST, MQTT_PORT))
+    try:
+        context = ssl.create_default_context()
+        broker_socket = context.wrap_socket(socket.socket(socket.AF_INET, socket.SOCK_STREAM), server_hostname=BROKER_HOST)
+        broker_socket.connect((BROKER_HOST, BROKER_PORT))
 
-    def forward(src, dst):
-        try:
-            while True:
-                data = src.recv(4096)
-                if not data: break
-                dst.sendall(data)
-        except: pass
+        def forward(src, dst):
+            try:
+                while True:
+                    data = src.recv(4096)
+                    if not data: break
+                    dst.sendall(data)
+            except:
+                pass
 
-    t1 = threading.Thread(target=forward, args=(client_socket, broker_socket))
-    t2 = threading.Thread(target=forward, args=(broker_socket, client_socket))
-    t1.start(); t2.start()
-    t1.join(); t2.join()
-    client_socket.close(); broker_socket.close()
+        t1 = threading.Thread(target=forward, args=(client_socket, broker_socket))
+        t2 = threading.Thread(target=forward, args=(broker_socket, client_socket))
+        t1.start(); t2.start()
+        t1.join(); t2.join()
+    finally:
+        client_socket.close()
+        broker_socket.close()
 
-server = socket.socket()
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind((LISTEN_HOST, LISTEN_PORT))
 server.listen(5)
-print(f"TCP proxy listening on {LISTEN_HOST}:{LISTEN_PORT}")
+print(f"Proxy listening on {LISTEN_HOST}:{LISTEN_PORT}")
 
 while True:
     client_sock, addr = server.accept()
